524307ad4ea4ff63a88f2de5b2764e8e03d8a1e1
铛铛~ <449828797@qq.com>
2018-08-05 20:15:26 +0800

    Initial commit

---
+++ src/hello.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class hello {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(1);
+        list.forEach(cc-> System.out.println(cc));
+    }
+}
